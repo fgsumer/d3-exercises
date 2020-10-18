e8826b67374348436d984ddd67cce94391345742
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-10-18 22:32:43 +0200

    debug lon/lat, add style, use bigger dataset

--- map/draw-map.js
+++ map/draw-map.js
@@ -1,66 +1,71 @@
 async function drawMap() {
   // 1. Access data
   let json = await d3.json('./us-states.json');
-  let dataset = await d3.csv('./zipcodes_1000.csv');
+  let dataset = await d3.csv('./zipcodes_all.csv');
 
   //console.log(json);
   //console.log(dataset);
 
-  // // 2. Create chart dimensions
-  // const width = d3.min([window.innerWidth * 0.8, window.innerHeight * 0.8]);
+  // 2. Create chart dimensions
 
-  // let dimensions = {
-  //   width: width,
-  //   height: width,
-  //   margin: {
-  //     top: 10,
-  //     right: 10,
-  //     bottom: 50,
-  //     left: 50,
-  //   },
-  // };
-  // dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
-  // dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: window.innerHeight * 0.9,
+    margin: {
+      top: 20,
+      left: 20,
+    },
+  };
+  dimensions.boundedWidth = dimensions.width - dimensions.margin.left * 2;
+  dimensions.boundedHeight = dimensions.height - dimensions.margin.top;
 
   const projection = d3
     .geoAlbersUsa()
-    .scale(1200)
-    .translate([960 / 1.8, 600 / 2]);
+    .scale(1300)
+    .translate([dimensions.boundedWidth / 2, dimensions.boundedHeight / 2]);
 
   const path = d3.geoPath().projection(projection);
 
-  const svg = d3.select('#wrapper').append('svg').attr('width', 960).attr('height', 600);
+  const svg = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('width', dimensions.boundedWidth)
+    .attr('height', dimensions.boundedHeight)
+    .attr('viewbox', `0, 0, ${dimensions.width}, ${dimensions.height}`);
 
-  const mapp = svg
-    .selectAll('path')
-    .data(json.features)
-    .enter()
-    .append('path')
-    .attr('d', path)
-    .attr('fill', '#ccc')
-    .attr('stroke', 'white')
-    .attr('stroke-linejoin', 'round');
+  // const mapp = svg
+  //   .selectAll('path')
+  //   .data(json.features)
+  //   .enter()
+  //   .append('path')
+  //   .attr('d', path)
+  //   .attr('stroke', 'white')
+  //   .attr('stroke-linejoin', 'round');
 
-  const bubbles = svg
-    .append('g')
-    .selectAll('circles')
+  const g = svg.append('g');
+
+  g.selectAll('circle')
     .data(dataset)
     .enter()
     .append('circle')
-    // .attr('cx', (d) => projection([d['lon'], d['lat']]))
-    .attr('cy', (d) => {
-      console.log(+d.lon, +d.lat);
-      let coords = projection([+d.lon, +d.lat]);
-      console.log(coords);
-      return coords[1];
-    })
-    .attr('cx', (d) => {
-      console.log(+d.lon, +d.lat);
-      let coords = projection([+d.lon, +d.lat]);
-      console.log(coords);
-      return coords[0];
-    })
-    .attr('r', 5);
+    .attr('cy', (d) => projection([-d.lon, +d.lat])[1])
+    .attr('cx', (d) => projection([-d.lon, +d.lat])[0])
+    .attr('r', 1.3)
+    .style('opacity', 0.3)
+    .style('fill', '#88CEC4');
+
+  svg.call(
+    d3
+      .zoom()
+      .extent([
+        [0, 0],
+        [dimensions.boundedWidth, dimensions.boundedHeight],
+      ])
+      .scaleExtent([1, 30])
+      .on('zoom', () => {
+        g.attr('transform', d3.event.transform);
+      }),
+  );
 }
 
 drawMap();
