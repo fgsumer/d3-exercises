9290aad28d4ff10fc7f67ac14148dff13e8726be
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-11-12 19:12:26 +0100

    add curtain and second map

--- map/draw-map.js
+++ map/draw-map.js
@@ -1,6 +1,6 @@
 async function drawMap() {
   // 1. Access data
-  let json = await d3.json('./us-states.json');
+  //let json = await d3.json('./us-states.json');
   let dataset = await d3.csv('./zipcodes_all.csv');
 
   //console.log(json);
@@ -12,7 +12,7 @@ async function drawMap() {
     width: window.innerWidth * 0.9,
     height: window.innerHeight * 0.9,
     margin: {
-      top: 20,
+      top: 0,
       left: 20,
     },
   };
@@ -22,7 +22,7 @@ async function drawMap() {
   const projection = d3
     .geoAlbersUsa()
     .scale(1300)
-    .translate([dimensions.boundedWidth / 2, dimensions.boundedHeight / 2]);
+    .translate([dimensions.boundedWidth / 2, dimensions.boundedHeight / 2.3]);
 
   const path = d3.geoPath().projection(projection);
 
@@ -31,16 +31,23 @@ async function drawMap() {
     .append('svg')
     .attr('width', dimensions.boundedWidth)
     .attr('height', dimensions.boundedHeight)
-    .attr('viewbox', `0, 0, ${dimensions.width}, ${dimensions.height}`);
+    .attr('viewbox', `0, 0, ${dimensions.boundedWidth}, ${dimensions.boundedHeight}`);
 
-  // const mapp = svg
+  // const map = svg
   //   .selectAll('path')
   //   .data(json.features)
   //   .enter()
   //   .append('path')
   //   .attr('d', path)
-  //   .attr('stroke', 'white')
-  //   .attr('stroke-linejoin', 'round');
+  //   .attr('stroke', 'null')
+  //   .attr('stroke-width', 'null')
+  //   .attr('stroke-linejoin', 'round')
+  //   .on('mouseover', function () {
+  //     d3.select(this).attr('stroke-width', '1').attr('stroke', '#88CEC4');
+  //   })
+  //   .on('mouseout', function (d) {
+  //     d3.select(this).attr('stroke-width', 'null').attr('stroke', 'null');
+  //   });
 
   const g = svg.append('g');
 
@@ -54,18 +61,40 @@ async function drawMap() {
     .style('opacity', 0.3)
     .style('fill', '#88CEC4');
 
-  svg.call(
-    d3
-      .zoom()
-      .extent([
-        [0, 0],
-        [dimensions.boundedWidth, dimensions.boundedHeight],
-      ])
-      .scaleExtent([1, 30])
-      .on('zoom', () => {
-        g.attr('transform', d3.event.transform);
-      }),
-  );
+  // const myColor = d3
+  //   .scaleLinear()
+  //   .domain([1, 12, 23, 34, 45, 56, 57])
+  //   .range(['#85C3CE', '#324376', '#F5DD90', '#FCA10C', '#FE0014', '#7F008A']);
+
+  const myColor = d3.scaleSequential().domain([1, 58]).interpolator(d3.interpolateBrBG);
+
+  const svg2 = d3
+    .select('.main')
+    .append('svg')
+    .attr('width', dimensions.boundedWidth)
+    .attr('height', dimensions.boundedHeight)
+    .attr('viewbox', `0, 0, ${dimensions.boundedWidth}, ${dimensions.boundedHeight}`);
+
+  const g2 = svg2.append('g');
+
+  g2.selectAll('circle')
+    .data(dataset)
+    .enter()
+    .append('circle')
+    .attr('cy', (d) => projection([-d.lon, +d.lat])[1])
+    .attr('cx', (d) => projection([-d.lon, +d.lat])[0])
+    .attr('r', 1.8)
+    .style('opacity', 0.8)
+    .style('fill', (d) => myColor(+d.index));
+
+  const title = svg2
+    .append('text')
+    .attr('x', dimensions.boundedWidth / 2)
+    .attr('y', 22)
+    .attr('fill', '19223B')
+    .style('font-size', '2em')
+    .attr('text-anchor', 'middle')
+    .text('USA map of ZIP codes by states');
 }
 
 drawMap();
